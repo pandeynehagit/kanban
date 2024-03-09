e99b82d35ff81a5e45f448502d0165da19cd8bac
pandeynehagit <nleetcode@gmail.com>
2024-03-09 23:50:41 +0530

    added active color functionality

--- kanban.js
+++ kanban.js
@@ -1,6 +1,7 @@
 let addbuttn = document.querySelector(".add-btn") ;
 let removebuttn = document.querySelector(".remove-btn");
 let modalcont = document.querySelector(".modal-cont");
+let allprioritycolor = document.querySelectorAll(".priority-color");
 let addTaskflag = false;
 let removeTaskflag = false;
 addbuttn.addEventListener("click",function(){
@@ -17,12 +18,23 @@ addbuttn.addEventListener("click",function(){
 removebuttn.addEventListener("click",function(){
     removeTaskflag=!removeTaskflag;
     if(removeTaskflag===true){
-        //make it flex
+        
         alert("remove button activated");
         removebuttn.style.color ="red";
     }
     else{
-        // none
+        
         removebuttn.style.color="white";
     }
 });
+allprioritycolor.forEach(function(colorele){
+    colorele.addEventListener("click",function(){
+        allprioritycolor.forEach(function(prioritycolorele){
+            prioritycolorele.classList.remove("active");
+        })
+        colorele.classList.add("active");
+    })
+});
+
+
+
